92b0072fc766014931473e32c93235399cf0a537
John Evans <john.g.evans.ne@gmail.com>
2015-12-28 20:35:22 -0500

    Add windows build.
    conda-build didn't like the default specification of "%CMAKE_GENERATORS%"
    so it had to be specifically set for VS 2015

---
+++ openjpeg/bld.bat
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+
+REM Configure step
+set CMAKE_CUSTOM=
+REM cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
+if errorlevel 1 exit 1
+
+REM Build step
+devenv OPENJPEG.sln /Build "%RELEASE_TARGET%"
+if errorlevel 1 exit 1
+
+REM Install step
+devenv OPENJPEG.sln /Build "%RELEASE_TARGET%" /Project INSTALL
+if errorlevel 1 exit 1
